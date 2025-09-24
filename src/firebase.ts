9a2b02db1c912028edf87b3b14650f288af3c2f0
Rushat Sharma <rushatsharma2501@gmail.com>
2025-09-25 03:11:28 +0530

    Deployment prep 1

--- src/firebase.ts
+++ src/firebase.ts
@@ -3,17 +3,15 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCvgTSDmUOMu715O2TZ-ZU0-ZO7uOlBCHE",
-  authDomain: "nexus-957bc.firebaseapp.com",
-  projectId: "nexus-957bc",
-  storageBucket: "nexus-957bc.firebasestorage.app",
-  messagingSenderId: "256865452474",
-  appId: "1:256865452474:web:742993bcc8b362f5f51d83"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+appId: import.meta.env.VITE_FIREBASE_APP_ID,
+
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-// Export auth and db
 export const auth = getAuth(app);
 export const db = getFirestore(app);
