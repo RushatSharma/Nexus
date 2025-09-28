6636c9ddda7c2cc6ea110d9984fae3ba5a0bd7ae
Rushat Sharma <rushatsharma2501@gmail.com>
2025-09-29 02:00:09 +0530

    Illustration were changed 3

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,6 +1,6 @@
 import { Button } from '@/components/ui/button';
 import { Star } from 'lucide-react';
-import heroImage from '@/assets/Hero.png';
+import heroImage from '@/assets/hero.png';
 import { Link } from 'react-router-dom';
 import { AuroraTextEffect } from './AuroraTextEffect'; // Make sure this path is correct
 
